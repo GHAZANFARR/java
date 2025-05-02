3e2455116ea242e68fb189a98c1df3827565a150
Bhavesh <bhaveshverma101@gmail.com>
2025-05-02 23:00:33 +0530

    update1

---
+++ src/userInput.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class userInput {
+    public static void main(String[] args) { //sout
+        Scanner input = new Scanner(System.in);
+        System.out.println("please enter your name: ");
+        String name = input.nextLine(); //here  if we need a line or string, its next line.. if we just need the int in the whole input.. we take nextint()
+        System.out.println("Good morning " + name);
+        System.out.println(name + ", also tell me your age");
+        int age  = input.nextInt();
+        System.out.println(name + " you are " + age + " years old");
+    }
+}
