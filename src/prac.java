ca506c7c68f97345c3b5234b13fcc55ba2250942
Bhavesh <bhaveshverma101@gmail.com>
2025-05-05 02:21:22 +0530

    Merge remote-tracking branch 'origin/master'

--- src/prac.java
+++ src/prac.java
@@ -5,19 +5,20 @@ public class prac {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome To The Program");
-        System.out.println("Enter your number: ");
-        int N = scanner.nextInt();
-        loop(N);
+        System.out.println("Enter your first number: ");
+        int first = scanner.nextInt();
+        System.out.println("Enter your second number: ");
+        int second = scanner.nextInt();
+
     }
 
-    public static int loop(int N) {
-        int i = 1;
-        int factorial = 1;
+    public static int loop(int first, int second) {
+        int greater = Math.max(first, second);
 
-        while(i<=N) {
-            factorial = factorial * i;
-            i++;
+        while(true) {
+            if (greater % first == 0 && greater % second == 0) {
+                return greater;
+            }
         }
-        return factorial;
     }
 }
