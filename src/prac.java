2673e90b6af006fa9810acd73cf46927d2689215
Bhavesh <bhaveshverma101@gmail.com>
2025-05-04 20:02:05 +0530

    Merge remote-tracking branch 'origin/master'

--- src/prac.java
+++ src/prac.java
@@ -4,13 +4,14 @@ public class prac {
     public static void main(String[] args) {
         int i = 1;
         int N = input();
-        int sum = 1;
+        int sum = 0;
 
-        while (i <= N) {
-            sum *= i;
-            i++;
+        while (N != 0) {
+            int f = N % 10;
+            sum += f;
+            N /= 10;
         }
-        System.out.println("sum is: " + sum);
+        System.out.println("sum od all digit of num is: " + sum);
     }
 
     public static int input(){
