ae77b71b5f84a655203aeaff7af182ccfb7c828c
Bhavesh <bhaveshverma101@gmail.com>
2025-05-11 02:22:05 +0530

    Merge remote-tracking branch 'origin/master'

--- src/prac.java
+++ src/prac.java
@@ -2,17 +2,19 @@ import java.util.Scanner;
 
 public class prac {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome To The Program");
-        System.out.println("Enter your number: ");
-        int first = scanner.nextInt();
-        loop(first);
-    }
 
-    public static void loop(int first) {
-        for (int i=1; i <= first; i++){
-            System.out.println(first + "x" + i + "=" + (first * i));
+        System.out.println("enter the required size of array: ");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            System.out.print("enter the number in array: ");
+            System.out.println();
+            array[i] = scanner.nextInt();
         }
+
+
     }
 }
