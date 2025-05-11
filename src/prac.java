b986f41e4b1bbc50aa91ea4bd439a0d2180face0
Bhavesh <bhaveshverma101@gmail.com>
2025-05-11 23:54:24 +0530

    Merge remote-tracking branch 'origin/master'

--- src/prac.java
+++ src/prac.java
@@ -4,29 +4,27 @@ public class prac {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("enter the required size of array: ");
+        System.out.println("enter the size of the array: ");
         int size = scanner.nextInt();
 
-        int[] array = new int[size];
-
-        for (int i = 0; i < size; i++) {
-            System.out.print("enter the number in array: ");
-            System.out.println();
-            array[i] = scanner.nextInt();
-        }
+        int[] array= new int[size];
 
         int total = sum(array);
-        System.out.println("total is: " + total);
+        System.out.println("sum is: " + total);
 
-        System.out.println("average of the array is: " + (total / size));
+        int average = total / size;
+        System.out.println("average is: " + average);
 
     }
 
     public static int sum(int[] array) {
+
         int sum = 0;
-        for(int num : array) {
+        for (int num : array) {
             sum += num;
         }
         return sum;
-    }
+}
+
+
 }
