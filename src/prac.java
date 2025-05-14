f68fbf749760ef2c3d438e5202fa04fc5a85f034
Bhavesh <bhaveshverma101@gmail.com>
2025-05-15 02:21:36 +0530

    Merge remote-tracking branch 'origin/master'

--- src/prac.java
+++ src/prac.java
@@ -2,20 +2,50 @@ import java.util.Scanner;
 
 public class prac {
     public static void main(String[] args) {
+        System.out.println("enter the size of the array: ");
+
         Scanner scanner = new Scanner(System.in);
+        int arrSize = scanner.nextInt();
 
-        System.out.println("enter the array size: ");
-        int size = scanner.nextInt();
+        int[] array = new int[arrSize];
 
-        int[] array = new int[size];
-
-        for (int i = 0; i < size; i++) {
-            System.out.println("enter the numbers of array: ");
+        for(int i = 0; i < array.length; i++) {
+            System.out.println("enter the number: ");
             array[i] = scanner.nextInt();
         }
+        System.out.println("enter the unwanted number: ");
+        int unNum = scanner.nextInt();
 
+        int occ = occ(array, unNum);
+        System.out.println("number of reappear: ");
 
+        int[] newArray = newArr(array, unNum, occ);
     }
 
-    public static boolean inc(inr[] array) {}
+    public static int occ(int[] array, int unNum) {
+
+        int occ = 0;
+
+        for (int i = 0; i < array.length ; i++) {
+            if (array[i] == unNum) {
+                occ++;
+            }
+        }
+
+        return occ;
+    }
+
+    public static int[] newArr(int[] array, int unNum, int occ) {
+
+        int[] newArray = new int[array.length-occ];
+        int index = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != unNum) {
+                newArray[index++] =   array [i];
+            }
+        }
+
+        return newArray;
+    }
 }
