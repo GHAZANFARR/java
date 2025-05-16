a9bb71c804628e627ae9121a63b6e0a03abaa330
Bhavesh <bhaveshverma101@gmail.com>
2025-05-16 17:43:33 +0530

    Merge remote-tracking branch 'origin/master'

--- src/prac.java
+++ src/prac.java
@@ -9,43 +9,11 @@ public class prac {
 
         int[] array = new int[arrSize];
 
-        for(int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length; i++) {
             System.out.println("enter the number: ");
             array[i] = scanner.nextInt();
         }
-        System.out.println("enter the unwanted number: ");
-        int unNum = scanner.nextInt();
 
-        int occ = occ(array, unNum);
-        System.out.println("number of reappear: ");
-
-        int[] newArray = newArr(array, unNum, occ);
-    }
-
-    public static int occ(int[] array, int unNum) {
-
-        int occ = 0;
-
-        for (int i = 0; i < array.length ; i++) {
-            if (array[i] == unNum) {
-                occ++;
-            }
-        }
-
-        return occ;
-    }
-
-    public static int[] newArr(int[] array, int unNum, int occ) {
-
-        int[] newArray = new int[array.length-occ];
-        int index = 0;
-
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] != unNum) {
-                newArray[index++] =   array [i];
-            }
-        }
-
-        return newArray;
+        for int
     }
 }
