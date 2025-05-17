dc36547592e7278559a25c20bc591ea7c17c6e04
Bhavesh <bhaveshverma101@gmail.com>
2025-05-17 14:55:51 +0530

    Merge remote-tracking branch 'origin/master'

--- src/prac.java
+++ src/prac.java
@@ -1,19 +1,33 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class prac {
     public static void main(String[] args) {
-        System.out.println("enter the size of the array: ");
-
         Scanner scanner = new Scanner(System.in);
-        int arrSize = scanner.nextInt();
 
-        int[] array = new int[arrSize];
+        System.out.println("******Welcome to the reverse array generator******");
 
-        for (int i = 0; i < array.length; i++) {
+        int sizeArray;
+        System.out.println("enter the size of the array: ");
+        sizeArray = scanner.nextInt();
+
+        int[] array = new int[sizeArray];
+        for(int i=0; i < sizeArray; i++) {
             System.out.println("enter the number: ");
             array[i] = scanner.nextInt();
         }
 
-        for int
+        int[] reverseArray = reverseArray(array);
+        System.out.println(Arrays.toString(reverseArray));
+    }
+
+    public static int[] reverseArray(int[] array) {
+
+        int[] reverse = new int[array.length];
+        int index = 0;
+        for(int i = array.length - 1; i >= 0; i--) {
+            reverse[index++] = array[i];
+        }
+        return reverse;
     }
 }
