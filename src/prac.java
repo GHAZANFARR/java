60963d87b957ee0a27ece296bc77f567bfa034af
Bhavesh <bhaveshverma101@gmail.com>
2025-05-18 16:16:18 +0530

    Merge remote-tracking branch 'origin/master'

--- src/prac.java
+++ src/prac.java
@@ -4,30 +4,30 @@ import java.util.Scanner;
 public class prac {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int[] array1 = arrayInp(1);
+        int[] array2 = arrayInp(2);
 
-        System.out.println("******Welcome to the reverse array generator******");
-
-        int sizeArray;
-        System.out.println("enter the size of the array: ");
-        sizeArray = scanner.nextInt();
-
-        int[] array = new int[sizeArray];
-        for(int i=0; i < sizeArray; i++) {
-            System.out.println("enter the number: ");
-            array[i] = scanner.nextInt();
-        }
-
-        int[] reverseArray = reverseArray(array);
-        System.out.println("reverse array is: "+ Arrays.toString(reverseArray));
+        sortArr(array1);
+        sortArr(array2);
     }
 
-    public static int[] reverseArray(int[] array) {
+    public static int[] arrayInp(int arrayNum) {
+        Scanner scanner = new Scanner(System.in);
 
-        int[] reverse = new int[array.length];
-        int index = 0;
-        for(int i = array.length - 1; i >= 0; i--) {
-            reverse[index++] = array[i];
+        System.out.println("enter the array size: ");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("enter the num: ");
+            array[i] = scanner.nextInt();
         }
-        return reverse;
+        return array;
+    }
+
+    public static void sortArr(int[] arr) {
+
+        Arrays.sort(arr);
     }
 }
