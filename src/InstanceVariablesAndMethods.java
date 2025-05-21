0261c38c8872688faa3fd5bf2380484b42f51dec
Bhavesh <bhaveshverma101@gmail.com>
2025-05-21 21:49:05 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/InstanceVariablesAndMethods.java
@@ -0,0 +1,44 @@
+public class InstanceVariablesAndMethods { //taking it for a car as an example
+
+    int noOfWheels;
+
+    String color;
+
+    int noOfSeats;
+
+    float currentFuelInLiters;
+
+    float currentSpeed;
+
+    float maxSpeed;
+
+    String fuelType;
+
+    public void drive(){
+        System.out.println("The Car is driving");
+        currentFuelInLiters--;
+    }
+
+    public void addFuel(float fuel){
+        currentFuelInLiters += fuel;
+    }
+
+    public float getCurrentFuel() {
+        System.out.println("current fuel is: " + currentFuelInLiters);
+        return currentFuelInLiters;
+    }
+
+    public void acceleration(float posSpeed){
+        currentSpeed += posSpeed;
+    }
+
+    public void deceleration(float negSpeed) {
+        currentSpeed -= negSpeed;
+    }
+
+    public float getCurrentSpeed(){
+        System.out.println("current speed is: " + currentSpeed);
+        return currentSpeed;
+    }
+
+}
