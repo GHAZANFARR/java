64b6d2be6537fe131107da3948138d9734edce67
Bhavesh <bhaveshverma101@gmail.com>
2025-05-26 02:38:11 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/Cars.java

--- src/Cars.java
+++ src/Cars.java
@@ -20,7 +20,7 @@ public class Cars { // Instance Variables And Methods example
 
      float maxSpeed;
 
-     String fuelType;
+     String fuelType; // not recommended to finalize();
 
      private static final float FUEL_COST_PER_LITER = 110.2f;
 
