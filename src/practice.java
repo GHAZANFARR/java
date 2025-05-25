5ee650e346e5f3e40faa3c78d17b4fb06011c1bf
Bhavesh <bhaveshverma101@gmail.com>
2025-05-25 20:36:58 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/Cars.java

--- src/practice.java
+++ src/practice.java
@@ -1,9 +1,33 @@
+import java.util.Scanner;
+
 public class practice {
     public static void main(String[] args) {
-        System.out.println("1" + 1);
-        System.out.println("1" + -1);
-        //System.out.println("1" + 1*1);
-        //.out.println("1" + 1/1);
-       // System.out.println("1" + 1%1);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("the limit of array: ");
+        int limit = scanner.nextInt();
+        int[] array = new int[limit];
+
+        for (int i = 0; i < limit; i++) {
+            System.out.print("enter the number: ");
+            array[i] = scanner.nextInt();
+        }
+
+        boolean repeat = false;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < args.length; j++) {
+                if (array[i] == array[j]) {
+                    repeat = true;
+                    break;
+                }
+            }
+        }
+
+
+        if (repeat) {
+            System.out.println("there was repeatation");
+        }else System.out.println("there was no repetation");
     }
 }
+
