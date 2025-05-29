6c52e18a2b41ee3758855aa508e9fa81934d354e
Bhavesh <bhaveshverma101@gmail.com>
2025-05-29 22:37:03 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/Cars.java

---
+++ src/Recursion.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Recursion {
+    public static void main(String[] args) {
+
+
+        int input = input();
+        int fact = recursionFactorial(input);
+        System.out.print("factorial is: " + fact);
+    }
+
+    public static int input() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("enter the number: ");
+
+        return scanner.nextInt();
+    }
+
+    public static int recursionFactorial(int input) {
+
+        if(input == 1) {
+            return 1;
+        }
+        return input * recursionFactorial(input-1);
+    }
+}
