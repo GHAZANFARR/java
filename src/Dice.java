f81939d5bcc926fe64155211214c881edcafc101
Bhavesh <bhaveshverma101@gmail.com>
2025-06-04 12:49:13 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/Cars.java

---
+++ src/Dice.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Dice {
+    public static void main(String[] args) {
+        System.out.println("welcome to the dice");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("do you want to roll the dice?: (Y/N)");
+        String task = scanner.nextLine();
+        boolean command = task.equalsIgnoreCase("Y");
+        if (command){
+            diceRoll();
+        }else System.out.println("bye");
+    }
+
+    public static void diceRoll() {
+        int result = (int)(Math.random() * 6);
+        System.out.println(result);
+    }
+}
---
+++ src/MathCircumfrence.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class MathCircumfrence {
+    public static void main(String[] args) {
+        System.out.println("enter the radius: ");
+        Scanner scanner = new Scanner(System.in);
+        double radius = scanner.nextInt();
+
+        double circumfrence = 2 * Math.PI * radius;
+        System.out.println(circumfrence);
+    }
+}
