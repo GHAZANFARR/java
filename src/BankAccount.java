b21015a10de700371e56eefa102157a6e84830ee
Bhavesh <bhaveshverma101@gmail.com>
2025-06-13 02:27:29 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/Cars.java

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -9,7 +9,6 @@ class Bank{
     float accountNumber;
     float requiredMoney;
 
-
     private float withdrawal(){
         balance -= requiredMoney;
         if (balance < 0){
@@ -18,16 +17,38 @@ class Bank{
         return balance;
     }
 
-    private String getUserName(Scanner scanner){
-        System.out.println("enter your name: ");
-        return scanner.nextLine().
+    public void getUserName(Scanner scanner){
+        System.out.print("enter your name: ");
+        this.userName = scanner.nextLine();
+    }
+
+    public void setUserName(String userName){
+        this.userName = userName;
+    }
+
+    public String getUserName(){
+        return this.userName;
     }
 }
 
 public class BankAccount {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Bank bank = new Bank();
 
+        bank.getUserName(scanner);
+        String username = bank.getUserName();
+        String formatUserName = nameMod(username);
+        if(!formatUserName.isEmpty()){
+            System.out.println("Welcome to the BANK, " + formatUserName + ".");
+        }
+    }
 
+    public static String nameMod(String username) {
+        if(username == null || username.isEmpty()){
+            System.err.println("ERROR: Name Field is Empty");
+             throw new IllegalArgumentException("username cannot be empty");
+        }
+        return username.substring(0,1).toUpperCase() + username.substring(1).toLowerCase();
     }
 }
