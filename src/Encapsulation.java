6f9d8e653cb7da6604aaf828341a56f5119d04ca
Bhavesh <bhaveshverma101@gmail.com>
2025-06-17 02:40:30 +0530

    revised

---
+++ src/Encapsulation.java
@@ -0,0 +1,47 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+class Bike{
+    private boolean keys;
+    private float fuelLevels;
+
+    public void initializeBike(Scanner scanner) {
+
+        this.driverKeys(scanner);
+        this.fuelLevels = getFuelLevels(scanner);
+    }
+
+    private Boolean driverKeys(Scanner scanner){
+        System.out.println("enter your key: (yes/no)");
+        this.keys = scanner.nextLine().equalsIgnoreCase("yes");
+         return this.keys;
+    }
+
+    private float getFuelLevels(Scanner scanner){
+        System.out.println("enter the fuel levels: ");
+        return scanner.nextFloat();
+    }
+
+    private boolean checkFuel(){
+        return this.fuelLevels > 0 ? true: false;
+    }
+
+
+    public Boolean bikeStart(){
+
+        return this.keys && checkFuel();
+    }
+}
+
+
+public class Encapsulation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Bike bike = new Bike();
+
+
+        bike.bikeStart();
+        String bikeBehave = bike.bikeStart() ? "bike is started" : "bike is at rest";
+    }
+}
--- src/TernaryOperator.java
+++ src/TernaryOperator.java
@@ -15,7 +15,7 @@ public class TernaryOperator {
     public static void maxMin(int first, int second) {
 
         //suggesting to use math library
-        int     maxMin = first < second ? first : second;
+        int maxMin = first < second ? first : second;
         System.out.println(maxMin + " is the minimum number of the two");
     }
 }
