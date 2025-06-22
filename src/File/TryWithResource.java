e1b145fcf8fa072db912852166acbe3803917117
Bhavesh <bhaveshverma101@gmail.com>
2025-06-22 10:41:16 +0530

    revised

--- src/File/TryWithResource.java
+++ src/File/TryWithResource.java
@@ -1,24 +1,17 @@
 package File;
 
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class Writer {
+public class TryWithResource {
     public static void main(String[] args) {
         String fileName = "java-test.txt";
-        FileWriter write = null;
-        try{
-            write = new FileWriter(fileName);
+        try(FileWriter write = new FileWriter(fileName)){ //automatically handle the exceptions in within the bracket
             write.write("My name is Optimus Prime.");
             write.flush();
             System.out.println("text has been saved successfully");
         } catch (IOException e) {
             System.out.printf("something went wrong %s\n", e.getMessage());
-        }finally {
-            if (write != null){
-                write.close();
-            }
-        }
+        } //no need to finally in try-with-resource
     }
 }
