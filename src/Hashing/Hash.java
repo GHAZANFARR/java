fec6b923aba4de62a7d4bb0ee6edda91bf245200
Bhavesh <bhaveshverma101@gmail.com>
2025-07-01 03:22:44 +0530

    revised

---
+++ src/Hashing/Hash.java
@@ -0,0 +1,13 @@
+package Hashing;
+
+public class Hash {
+    public static void main(String[] args) {
+
+
+        String key = "Orange";
+        int hashcode = key.hashCode();
+        System.out.println("Hash for orange is: " + hashcode);
+    }
+}
+
+
