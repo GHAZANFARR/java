663c93154535b1c22efaf36b4936e8e5bef0760e
Bhavesh <bhaveshverma101@gmail.com>
2025-07-01 13:24:49 +0530

    revised

---
+++ src/Hashing/AccountNum.java
@@ -0,0 +1,15 @@
+package Hashing;
+
+public class AccountNum {
+    private String AccountId;
+
+    public AccountNum(String AccountId) {
+        this.AccountId = AccountId;
+    }
+
+    public int hash(){
+        return AccountId.hashCode();
+    }
+
+    if
+}
