4e115c05901c9b1e6ef6a486324586d3993b78cf
Bhavesh <bhaveshverma101@gmail.com>
2025-07-18 17:42:48 +0530

    revised

--- src/interview/Palindrome.java
+++ src/interview/Palindrome.java
@@ -6,7 +6,6 @@ public class Palindrome {
         Scanner scanner = new Scanner(System.in);
         System.out.println("enter the string: ");
         String check = scanner.nextLine();
-
         String reverse = new StringBuilder(check).reverse().toString();
 
         System.out.println(Boolean.parseBoolean(check = reverse) ? "palindrome" : "not palindrome");
